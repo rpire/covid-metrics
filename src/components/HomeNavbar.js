84f019c1f0de131cc92815654916225a8e80883f
rpire <rubenpire7@gmail.com>
2021-10-13 16:32:38 -0400

    Implement search bar in HomeNavbar.js

--- src/components/HomeNavbar.js
+++ src/components/HomeNavbar.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { CgMenuGridR } from 'react-icons/cg';
+import { GoSearch } from 'react-icons/go';
 
-const HomeNavbar = () => (
-  <nav>
-    <span>UK</span>
-    <span>most cases</span>
-    <button type="button">
-      <CgMenuGridR />
-    </button>
-  </nav>
-);
+const HomeNavbar = (props) => {
+  const {
+    search,
+    handleChange,
+  } = props;
+
+  return (
+    <nav className="flex-box">
+      <span className="nav-title">UK</span>
+      <span className="nav-center">most cases</span>
+      <div className="input-container flex-box">
+        <input
+          type="text"
+          id="search"
+          name="search"
+          value={search}
+          onChange={handleChange}
+          placeholder="Search country"
+        />
+        <label htmlFor="search">
+          <GoSearch />
+        </label>
+      </div>
+    </nav>
+  );
+};
 
 export default HomeNavbar;
