251a144017869141506201d1f27172e65cee3fb0
rpire <rubenpire7@gmail.com>
2022-03-14 20:06:00 -0400

    Change string concatenation errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
       />
       <section className="main-container flex-box">
         <img
-          src={process.env.PUBLIC_URL + `/media/${data.id}.png`}
+          src={process.env.PUBLIC_URL.concat(`/media/${data.id}.png`)}
           alt={`Map silhouette for ${data.name}`}
           className="main-img"
         />
